0e5a311ce96ccc4e8c6290a636afb6040ee4d727
frankt117 <frankjtorresjr@gmail.com>
2014-05-21 15:48:25 -0500

    Initial Commit - Problems 1 and 2 solved.

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+/**
+ * Created by torrefra on 4/15/2014.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Problems fun = new Problems();
+//
+//        int[] arrA = new int[100];
+//        int[] arrB = new int[100];
+//        fun.evenFibonacciNumbers(1,2, arrA);
+//        fun.arrayPrint(arrA);
+//        arrB = fun.evenValues(arrA);
+//        fun.arrayPrint(arrB);
+//        int answer = fun.sumArray(arrB);
+//
+//        System.out.format("%d%n",answer);
+
+        //fun.findPrimeFactors(13195);
+
+
+        //fun.getPrimeNumbers(330);
+        fun.findPrimeFactors(330);
+        //fun.findPrimeFactors(600851475143L);
+    }
+}
---
+++ src/Problems.java
@@ -0,0 +1,148 @@
+import java.util.*;
+
+/**
+ * Created by torrefra on 4/16/2014.
+ */
+public class Problems {
+
+    public void multiplesOf3And5()
+    {
+        int[] arr = new int[1000];
+        int arrayPos = 0;
+        for (int i = 0; i < 1000; i++) {
+            if(i%3 == 0  || i%5 == 0) {
+                //System.out.format("%d%n",i);
+                arr[arrayPos] = i;
+                arrayPos++;
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < 1000; i++) {
+            System.out.format("%d%n",arr[i]);
+            answer += arr[i];
+        }
+
+        System.out.format("ANSWER = %d%n",answer);
+    }
+
+    public int[] evenFibonacciNumbers (int first, int second, int[] arrA)
+    {
+        if(first > 4000000) {
+            return arrA;
+        }
+
+        System.out.format("%d%n",first);
+        this.arrayPush(arrA,first);
+        return this.evenFibonacciNumbers(second, first+second, arrA);
+    }
+
+    public int sumArray (int[] arrA)
+    {
+        int answer = 0;
+        for (int i = 0; i < arrA.length; i++) {
+            //System.out.format("%d%n",arr[i]);
+            answer += arrA[i];
+        }
+        return answer;
+    }
+
+    public int[] arrayPush (int[] arrA, int value)
+    {
+        for (int i = 0; i < arrA.length; i++) {
+            if(arrA[i] == 0) {
+                arrA[i] = value;
+                return arrA;
+            }
+        }
+
+        return arrA;
+    }
+
+    public void arrayPrint (int[] arrA)
+    {
+        for (int i = 0; i < arrA.length; i++) {
+            System.out.format("%d%n",arrA[i]);
+        }
+    }
+
+    public int[] evenValues (int[] arrA)
+    {
+        int[] arrB = new int[arrA.length];
+        int b = 0;
+
+        for (int i = 0; i < arrA.length; i++) {
+            if(arrA[i] % 2 == 0) {
+                arrB[b] = arrA[i];
+                b++;
+            }
+        }
+
+        return arrB;
+    }
+
+    public int[] getPrimeNumbersList ()
+    {
+        int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
+
+        return primes;
+    }
+
+    public int findLargestPrimeNumber ( int startNum )
+    {
+        for (int i = startNum; i > 0; i--) {
+            float div2 = (float)i/(float)2.0;
+            float div3 = (float)i/(float)3.0;
+            if ( div2%1 != 0 && div3%1 != 0 && i/i == 1) {
+                System.out.format("%d%n",i);
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    public List<Double> getPrimeNumbers ( long lastNum )
+    {
+        List<Double> primes = new ArrayList<Double>();
+        primes.add((double)2);
+        primes.add((double)3);
+
+        for (long i = 5; i < lastNum; i++) {
+            double div2 = (double)i/(double)2.0;
+            double div3 = (double)i/(double)3.0;
+            if ( div2%1 != 0 && div3%1 != 0 && i/i == 1) {
+                System.out.format("%d%n",i);
+                primes.add((double)i);
+            }
+        }
+
+        System.out.format("%n%n%n%n%n%n%n");
+        return primes;
+    }
+
+    public void findPrimeFactors ( long num )
+    {
+        List<Double> primes = new ArrayList<Double>();
+        primes = this.getPrimeNumbers(num);
+
+        //double[] primesArr = primes.toArray();
+
+        for (int i = 0; i < primes.size(); i++) {
+            if( num == 1 ) {
+                return;
+            }
+            boolean stay = true;
+            while(stay) {
+                double prime = (Double)primes.get(i).doubleValue();
+                double temp = (double)num / prime;
+                if(temp % 1 == 0) {
+                    System.out.format("%d%n",(long)prime);
+                    num = (long)temp;
+                } else {
+                    stay = false;
+                }
+            }
+        }
+    }
+}
